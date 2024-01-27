f5fa81cbcd347e25dac8e656154401985d5cac91
Yifei Li 李逸飞 <liyifei723@gmail.com>
2024-01-27 16:21:43 -0600

    ros node for reading sensor data from arduino

---
+++ src/rov_visualize/src/sensor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+import serial
+import json
+
+def read_serial_data(serial_connection):
+    try:
+        if serial_connection.in_waiting > 0:
+            return serial_connection.readline().decode('utf-8').rstrip()
+    except:
+        rospy.logerr("Failed to read from serial port")
+    return None
+
+def main():
+    rospy.init_node('json_to_twist', anonymous=True)
+    twist_publisher = rospy.Publisher('twist', Twist, queue_size=10)
+    rate = rospy.Rate(10)  # 10 Hz
+
+    serial_port = rospy.get_param('~serial_port', '/dev/ttyACM0')
+    baud_rate = rospy.get_param('~baud_rate', 9600)
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    while not rospy.is_shutdown():
+        serial_data = read_serial_data(ser)
+        if serial_data:
+            try:
+                data = json.loads(serial_data)
+                yaw = data['yaw']
+                pitch = data['pitch']
+                roll = data['roll']
+
+                # Create Twist message
+                twist_msg = Twist()
+                twist_msg.angular.x = roll
+                twist_msg.angular.y = pitch
+                twist_msg.angular.z = yaw
+
+                twist_publisher.publish(twist_msg)
+
+            except json.JSONDecodeError:
+                rospy.logerr("Invalid JSON received.")
+            except KeyError:
+                rospy.logerr("JSON does not contain yaw, pitch, and roll.")
+
+        rate.sleep()
+
+    ser.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
