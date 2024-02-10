e385367c12fd4e8e10740a97b56170a5373927c2
Yifei Li 李逸飞 <liyifei723@gmail.com>
2024-02-10 14:57:03 -0600

    motor control arduino side

--- src/rov_mcu/src/motor_controls.cpp
+++ src/rov_mcu/src/motor_controls.cpp
@@ -1,33 +1,45 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <ArduinoJson.h>
 
 Servo motor1;
 
 void setup() {
-  motor1.attach(3);
+  motor1.attach(2);
+  // motor1.writeMicroseconds(1000);
+  // delay(5000);
   motor1.writeMicroseconds(1500); // Initialize motor1
-  Serial.begin(9600); // Start serial communication
+  // delay(5000);
+  Serial.begin(57600); // Start serial communication
+  // setup onboard led
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   if (Serial.available()) {
     // Read a string in format "x\n" over serial
     String input = Serial.readStringUntil('\n');
-    int x = input.toInt();
     
-    // Map the incoming speed to motor compatible speeds and write to motor
-    //int speed1 = map(x, -1, 1, 1100, 1900);
-    if (x < 1100  || x > 1900)
-    {
-      Serial.println("bad input");
-    }
-    else
-    {
-    motor1.writeMicroseconds(x);
-    Serial.print("wrote speed1: ");
-    Serial.print(x);
-    Serial.println(" to motor1");
-    }
+    // pharse json string
+    // {"contorls": {"linear": {"x": 0.0, "y": 0.0, "z": 0.0}, "angular": {"x": 0.0, "y": 0.0, "z": 0.0}}}
+
+    // Parse the JSON object
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, input);
+    JsonObject controls = doc["controls"];
+    JsonObject linear = controls["linear"];
+    float x = linear["x"];
+    // add more code to parse y and z
+
+    // map -1 ~ 1 to 1100 ~ 1900 without map()
+    int x_mapped = (int)(x * 500 + 1500);
+    
+
+    // display x raw value on onboard led
+    digitalWrite(LED_BUILTIN, x > 0 ? HIGH : LOW);
+    
+    // Write the mapped value to motor1
+    motor1.writeMicroseconds(2000);
 
   }
 }
