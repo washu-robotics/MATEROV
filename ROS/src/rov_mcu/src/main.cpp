cf774fb8b6f95d3103bcc098af489669a67da123
Yifei Li 李逸飞 <liyifei723@gmail.com>
2024-02-24 12:09:26 -0600

    write sensro datta to serial

--- ROS/src/rov_mcu/src/main.cpp
+++ ROS/src/rov_mcu/src/main.cpp
@@ -1,6 +1,17 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050.h" // not necessary if using MotionApps include file
+#include <Arduino_JSON.h>
+
+#include <Adafruit_LPS35HW.h>
+
+Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
+// For SPI mode, we need a CS pin
+#define LPS_CS  10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LPS_SCK  13
+#define LPS_MISO 12
+#define LPS_MOSI 11
+
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -74,14 +85,15 @@ void loop() {
 
         // display quaternion values in easy matrix form: w x y z
         mpu.dmpGetQuaternion(&q, fifoBuffer);
-        // Serial.print("quat\t");
-        // Serial.print(q.w);
-        // Serial.print("\t");
-        // Serial.print(q.x);
-        // Serial.print("\t");
-        // Serial.print(q.y);
-        // Serial.print("\t");
-        // Serial.println(q.z);
+        /* Serial.print("quat\t");
+        Serial.print(q.w);
+        Serial.print("\t");
+        Serial.print(q.x);
+        Serial.print("\t");
+        Serial.print(q.y);
+        Serial.print("\t");
+        Serial.println(q.z);
+        */
 
         mpu.dmpGetGravity(&gravity, &q);
 
@@ -89,39 +101,99 @@ void loop() {
         // and rotated based on known orientation from quaternion
         mpu.dmpGetAccel(&aa, fifoBuffer);
         mpu.dmpConvertToWorldFrame(&aaWorld, &aa, &q);
-        // Serial.print("aworld\t");
-        // Serial.print(aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
-        // Serial.print("\t");
-        // Serial.print(aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
-        // Serial.print("\t");
-        // Serial.println(aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        /*Serial.print("aworld\t");
+        Serial.print(aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        Serial.print("\t");
+        Serial.print(aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        Serial.print("\t");
+        Serial.println(aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        */
 
         // display initial world-frame acceleration, adjusted to remove gravity
         // and rotated based on known orientation from quaternion
         mpu.dmpGetGyro(&gg, fifoBuffer);
         mpu.dmpConvertToWorldFrame(&ggWorld, &gg, &q);
-        // Serial.print("ggWorld\t");
-        // Serial.print(ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD);
-        // Serial.print("\t");
-        // Serial.print(ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD);
-        // Serial.print("\t");
-        // Serial.println(ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        /* Serial.print("ggWorld\t");
+        Serial.print(ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        Serial.print("\t");
+        Serial.print(ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        Serial.print("\t");
+        Serial.println(ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        */
 
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        // Serial.print("ypr\t");
-        // Serial.print(ypr[0] * RAD_TO_DEG);
-        // Serial.print("\t");
-        // Serial.print(ypr[1] * RAD_TO_DEG);
-        // Serial.print("\t");
-        // Serial.println(ypr[2] * RAD_TO_DEG);
+        /*Serial.print("ypr\t");
+        Serial.print(ypr[0] * RAD_TO_DEG);
+        Serial.print("\t");
+        Serial.print(ypr[1] * RAD_TO_DEG);
+        Serial.print("\t");
+        Serial.println(ypr[2] * RAD_TO_DEG);
         
-        // Serial.println();
-
-        // encode yaw/pitch/roll into json string
-        // Serial.println("{\"yaw\":" + String(ypr[0] * RAD_TO_DEG) + ",\"pitch\":" + String(ypr[1] * RAD_TO_DEG) + ",\"roll\":" + String(ypr[2] * RAD_TO_DEG) + "}");
+        Serial.println();
+        */
 
         // encode all data satisfying ros sensor_msgs/Imu.msg into json string
-        Serial.println("{\"orientation\":{\"w\":" + String(q.w) + ",\"x\":" + String(q.x) + ",\"y\":" + String(q.y) + ",\"z\":" + String(q.z) + "},\"angular_velocity\":{\"x\":" + String(ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD) + ",\"y\":" + String(ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD) + ",\"z\":" + String(ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD) + "},\"linear_acceleration\":{\"x\":" + String(aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + ",\"y\":" + String(aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + ",\"z\":" + String(aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + "}}");
+        //Serial.println("{\"orientation\":{\"w\":" + String(q.w) + ",\"x\":" + String(q.x) + ",\"y\":" + String(q.y) + ",\"z\":" + String(q.z) + "},\"angular_velocity\":{\"x\":" + String(ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD) + ",\"y\":" + String(ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD) + ",\"z\":" + String(ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD) + "},\"linear_acceleration\":{\"x\":" + String(aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + ",\"y\":" + String(aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + ",\"z\":" + String(aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2) + "}}");
+
+        // data to send
+        /*
+        {
+            "imu": {
+                "orientation": {
+                "w": "",
+                "x": "",
+                "y": "",
+                "z": ""
+                },
+                "angular_velocity": {
+                "x": "",
+                "y": "",
+                "z": ""
+                },
+                "linear_acceleration": {
+                "x": "",
+                "y": "",
+                "z": ""
+                }
+            },
+            "pressure": {
+                "temperature": "",
+                "pressure": ""
+            }
+        }
+        */
+        float imu_orientation_w = q.w;
+        float imu_orientation_x = q.x;
+        float imu_orientation_y = q.y;
+        float imu_orientation_z = q.z;
+
+        float imu_angular_velocity_x = ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD;
+        float imu_angular_velocity_y = ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD;
+        float imu_angular_velocity_z = ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD;
+
+        float imu_linear_acceleration_x = aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+        float imu_linear_acceleration_y = aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+        float imu_linear_acceleration_z = aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+
+        float pressure_temperature = lps35hw.readTemperature();
+        float pressure_pressure = lps35hw.readPressure();
+
+        JSONVar data;
+        data["imu"]["orientation"]["w"] = imu_orientation_w;
+        data["imu"]["orientation"]["x"] = imu_orientation_x;
+        data["imu"]["orientation"]["y"] = imu_orientation_y;
+        data["imu"]["orientation"]["z"] = imu_orientation_z;
+        data["imu"]["angular_velocity"]["x"] = imu_angular_velocity_x;
+        data["imu"]["angular_velocity"]["y"] = imu_angular_velocity_y;
+        data["imu"]["angular_velocity"]["z"] = imu_angular_velocity_z;
+        data["imu"]["linear_acceleration"]["x"] = imu_linear_acceleration_x;
+        data["imu"]["linear_acceleration"]["y"] = imu_linear_acceleration_y;
+        data["imu"]["linear_acceleration"]["z"] = imu_linear_acceleration_z;
+        data["pressure"]["temperature"] = pressure_temperature;
+        data["pressure"]["pressure"] = pressure_pressure;
+
+        String jsonString = JSON.stringify(data);
+        Serial.println(jsonString);
 
         delay(20);
     }
