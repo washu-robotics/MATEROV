07a73dacdd208e43531ae8e579f45a5eb03ecc5a
Yifei Li 李逸飞 <liyifei723@gmail.com>
2024-03-02 14:50:43 -0600

    mcu code

--- ROS/src/rov_mcu/src/main.cpp
+++ ROS/src/rov_mcu/src/main.cpp
@@ -1,6 +1,6 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-#include <Arduino_JSON.h>
+#include <ArduinoJson.h>
 
 #include <Servo.h>
 
@@ -13,7 +13,6 @@ Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
 #define LPS_SCK  13
 #define LPS_MISO 12
 #define LPS_MOSI 11
-
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -46,6 +45,11 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
+void motorSetup() {
+  motor1.attach(3);
+  motor1.writeMicroseconds(1500);
+}
+
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -55,6 +59,9 @@ void setup() {
     #endif
 
     Serial.begin(19200);
+    Serial.setTimeout(50);
+
+    motorSetup();
 
     mpu.initialize();
     devStatus = mpu.dmpInitialize();
@@ -80,10 +87,26 @@ void setup() {
         dmpReady = true;
         packetSize = mpu.dmpGetFIFOPacketSize();
     }
+}
 
+int processThrusterCmd(double x_vel, double y_vel, double z_vel) {
 
-    motor1.attach(3);
-    motor1.writeMicroseconds(1500); // Initialize motor1
+    // if any of the values are nan, return 1500
+    x_vel = isnan(x_vel) ? 0 : x_vel;
+    y_vel = isnan(y_vel) ? 0 : y_vel;
+    z_vel = isnan(z_vel) ? 0 : z_vel;
+    
+
+  int x_thruster = (int)(x_vel * 500 + 1500); // 1000 - 2000
+  if (x_thruster > 2000) {
+    x_thruster = 2000;
+  } else if (x_thruster < 1000) {
+    x_thruster = 1000;
+  }
+
+  motor1.writeMicroseconds(x_thruster);
+  Serial.println(x_thruster);
+  return x_thruster;
 }
 
 void loop() {
@@ -185,60 +208,48 @@ void loop() {
         float pressure_temperature = lps35hw.readTemperature();
         float pressure_pressure = lps35hw.readPressure();
 
-        JSONVar data;
-        data["imu"]["orientation"]["w"] = imu_orientation_w;
-        data["imu"]["orientation"]["x"] = imu_orientation_x;
-        data["imu"]["orientation"]["y"] = imu_orientation_y;
-        data["imu"]["orientation"]["z"] = imu_orientation_z;
-        data["imu"]["angular_velocity"]["x"] = imu_angular_velocity_x;
-        data["imu"]["angular_velocity"]["y"] = imu_angular_velocity_y;
-        data["imu"]["angular_velocity"]["z"] = imu_angular_velocity_z;
-        data["imu"]["linear_acceleration"]["x"] = imu_linear_acceleration_x;
-        data["imu"]["linear_acceleration"]["y"] = imu_linear_acceleration_y;
-        data["imu"]["linear_acceleration"]["z"] = imu_linear_acceleration_z;
-        data["pressure"]["temperature"] = pressure_temperature;
-        data["pressure"]["pressure"] = pressure_pressure;
+        StaticJsonDocument<1024> response;
 
-        String jsonString = JSON.stringify(data);
-        // Serial.println(jsonString);
+        response["imu"]["orientation"]["w"] = imu_orientation_w;
+        response["imu"]["orientation"]["x"] = imu_orientation_x;
+        response["imu"]["orientation"]["y"] = imu_orientation_y;
+        response["imu"]["orientation"]["z"] = imu_orientation_z;
+        response["imu"]["angular_velocity"]["x"] = imu_angular_velocity_x;
+        response["imu"]["angular_velocity"]["y"] = imu_angular_velocity_y;
+        response["imu"]["angular_velocity"]["z"] = imu_angular_velocity_z;
+        response["imu"]["linear_acceleration"]["x"] = imu_linear_acceleration_x;
+        response["imu"]["linear_acceleration"]["y"] = imu_linear_acceleration_y;
+        response["imu"]["linear_acceleration"]["z"] = imu_linear_acceleration_z;
+        response["pressure"]["temperature"] = pressure_temperature;
+        response["pressure"]["pressure"] = pressure_pressure;
+
+        char buffer[1024];
+        serializeJson(response, buffer);
+        Serial.println(buffer);
         delay(20);
         
     }
+    
     if (Serial.available()) {
         // Read a string in format "x\n" over serial
-        String input = Serial.readStringUntil('\n');
 
-        Serial.println(input);
+        StaticJsonDocument<256> request;
+        String receivedData = Serial.readStringUntil('\n');
+        DeserializationError error = deserializeJson(request, receivedData);
 
-        // pharse json string
         // {"controls": {"linear": {"x": 0.0, "y": 0.0, "z": 0.0}, "angular": {"x": 0.0, "y": 0.0, "z": 0.0}}}
 
-        // Parse the JSON object
-        JSONVar data = JSON.parse(input);
-
-        // Serial.println(data);
-
-        Serial.println(data["controls"]["linear"]["x"]);
-
-
-        //Read pwm duty cycle
-        float linear_x = JSON.stringify(data["controls"]["linear"]["x"]).toFloat();
-
-        Serial.println(linear_x);
-
-        int x = (int)(linear_x * 500 + 1500);
-
-        if (x < 1000  || x > 2000)
-        {
-            Serial.println("bad input");
-        }
-        else
-        {
-            motor1.writeMicroseconds(x);
-            Serial.print("wrote speed1: ");
-            Serial.print(x);
-            Serial.println(" to motor1");
-        }
+        double x_vel = request["controls"]["linear"]["x"];
+        double y_vel = request["controls"]["linear"]["y"];
+        double z_vel = request["controls"]["linear"]["z"];
+        Serial.println(x_vel);
+        int x_thruster = processThrusterCmd(x_vel, y_vel, z_vel);
     }
+    else {
+        // Serial.println("no incoming data or no serial connection established");
+        processThrusterCmd(0, 0, 0);
+    }
+    
 
 }
+
